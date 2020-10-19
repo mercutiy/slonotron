21895c079303b817d7f37a57f01de418db8decd3
miky <druid>
2020-10-19 14:37:02 +0200

    Initial commit

---
+++ st/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+from . import views
+from django.contrib.auth import views as auth_views
+
+
+urlpatterns = [
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('parties/', views.parties, name='parties'),
+    path('party/<int:party_id>/', views.party, name='party'),
+    path('user/<int:user_id>/', views.user, name='user'),
+]
