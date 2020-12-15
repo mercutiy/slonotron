9827cfb7fb2023e24a68ea12417098fac2c988a4
miky <miky.podgornov@gmail.com>
2020-12-15 23:58:13 +0100

    Move db

--- slonotron/settings.py
+++ slonotron/settings.py
@@ -10,7 +10,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
-import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,7 +78,7 @@ WSGI_APPLICATION = 'slonotron.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'db' / 'db.sqlite3',
     }
 }
 
